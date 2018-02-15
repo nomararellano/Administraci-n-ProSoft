3abc431e70954115c9ad6987251f5d958cbebf49
CrisGuzmanSuarez <36390918+CrisGuzmanSuarez@users.noreply.github.com>
2018-02-15 00:22:36 -0600

    Numeros positivos.
    Modifiqué al programa para que ahora solamente calcule números enteros positivos.

--- mayor.c
+++ mayor.c
@@ -3,19 +3,29 @@
 int main ()
 {
 	int num,n,mayor,i;
-	
+	printf("Buen dia, bienvenido al programa que calcula al valor mayor positivo.\n");
+
 	do {
 		printf ("\nNo. de valores: ");
 		scanf ("%d",&n);
 	} while (n <= 0);
+	
 	mayor= 0;
+	
 	for (i= 1; i <= n; i++) {
-		printf ("Valor: ");
-		scanf ("%d", &num);
-		if (num > mayor) mayor= num;
+
+		do {
+			printf ("Valor: ");
+			scanf ("%d", &num);
+
+			if (num <= 0){
+				printf("Error, por favor escribe numeros positivos.\n");
+			}
+
+		} while ( num <= 0 );
+
+		if (num > mayor) mayor = num;
 	}
-	printf ("El num. mayor es: %d\n",mayor);
+
+	printf ("El num. mayor es: %d\n", mayor);
 }
-
-
-
